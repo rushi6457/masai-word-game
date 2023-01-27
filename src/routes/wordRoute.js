777113b0d4b56aeee77ea1ca31c56e1920660cd5
rushi6457 <rushiypatil@gmail.com>
2023-01-27 14:04:42 +0530

    backend

--- src/routes/wordRoute.js
+++ src/routes/wordRoute.js
@@ -2,16 +2,16 @@
 const crypto = require('crypto');
 function GenerateRandomWord(req,res) {
   let word = "";
-  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let possible = "abcdefghijklmnopqrstuvwxyz";
    let length ;
     if(length !==0){
-       length= Math.floor(Math.random()*10-5)
+       length= Math.floor((Math.random()+2)*5)
     }
     
   for (let i = 0; i < length; i++) {
     word += possible.charAt(Math.floor(Math.random() * possible.length));
   }
-  if(word.length =0){
+  if(word.length ==0){
     res.send({"message":"Generate another word"})
   }
   res.send(word)
